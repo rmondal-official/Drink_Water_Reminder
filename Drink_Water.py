ce0854cebdc267891713df5b6c979f012a75ffff
Rajesh Mondal <78198704+rmondal-official@users.noreply.github.com>
2024-02-09 22:48:18 +0530

    Initial commit

--- Drink_Water.py
+++ Drink_Water.py
@@ -1,7 +1,7 @@
 
 # Project: "Drink Water Now"
-# It is water drinking notification remainder program. It will notify -
-# -you to drink water after one hour by using notification panel of windows.
+# It is water drinking notification remainder program. It will notify
+# you to drink water after one hour by using notification panel of windows.
 # Creation Date:-29/11/2020
 
 import time
@@ -9,6 +9,8 @@ from plyer import notification
 
 if __name__ == "__main__":
     while True:
-        notification.notify(title='Drink Water Now!!!', message='Your body is composed of about 60% water. The functions of these bodily fluids include digestion, absorption, circulation, creation of saliva, transportation of nutrients, and maintenance of body temperature.',  app_icon=r"C:\Users\Dell\Documents\Downloads\Desktop\Rajesh\Logo\glass.ico", timeout=8)
+        notification.notify(title='Drink Water Now!!!',
+                            message='Your body is composed of about 60% water. The functions of these bodily fluids include digestion, absorption, circulation, creation of saliva, transportation of nutrients, and maintenance of body temperature.',
+                            app_icon="your_notification_icon_image.ico", timeout=8)
         time.sleep(60*60)
 # Finish
